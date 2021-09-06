12996fc5ede16ef372827133cb59986e48fee023
Christian Müllenhagen <christian.muellenhagen@turbinekreuzberg.com>
2021-09-06 16:40:00 +0200

    add file to project root

--- src/main/java/org/jetbrains/plugins/template/actions/ExtendInPyzAction.java
+++ src/main/java/org/jetbrains/plugins/template/actions/ExtendInPyzAction.java
@@ -4,6 +4,10 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileFactory;
+import com.intellij.psi.impl.file.PsiDirectoryFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class ExtendInPyzAction extends AnAction
@@ -11,6 +15,13 @@ public class ExtendInPyzAction extends AnAction
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
+
+        final PsiFileFactory factory = PsiFileFactory.getInstance(project);
+        final PsiFile file = factory.createFileFromText("test.txt", "charSequence");
+
+        PsiDirectory baseDir = PsiDirectoryFactory.getInstance(project).createDirectory(project.getBaseDir());
+        baseDir.add(file);
         Messages.showMessageDialog(project,"Debug" , "Greeting", Messages.getInformationIcon());
+
     }
 }
