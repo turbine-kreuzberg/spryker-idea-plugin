477dc41c0c4b40706c2f0610272bbf2b8f24260c
Christian Müllenhagen <christian.muellenhagen@turbinekreuzberg.com>
2021-09-08 09:35:16 +0200

    clean up

--- src/main/java/org/jetbrains/plugins/template/actions/ExtendInPyzAction.java
+++ src/main/java/org/jetbrains/plugins/template/actions/ExtendInPyzAction.java
@@ -3,19 +3,13 @@ package org.jetbrains.plugins.template.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.fileEditor.impl.LoadTextUtil;
 import com.intellij.openapi.fileTypes.UnknownFileType;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
-import com.intellij.psi.PsiManager;
 import com.intellij.psi.impl.file.PsiDirectoryFactory;
 import com.intellij.util.ResourceUtil;
 import com.jetbrains.php.lang.PhpFileType;
@@ -23,68 +17,79 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 
 public class ExtendInPyzAction extends AnAction
 {
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-        Project project = e.getProject();
+    public void actionPerformed(@NotNull AnActionEvent actionEvent) {
+        Project project = actionEvent.getProject();
 
-        // dateiname = e.getData(PlatformDataKeys.VIRTUAL_FILE).getName();
-        // ordner = e.getData(PlatformDataKeys.VIRTUAL_FILE).getParent().getCanonicalPath()
-        // projektordner = project.getBasePath()
-
-        String originalPath = getClassPath(e);
+        VirtualFile currentFile = actionEvent.getData(PlatformDataKeys.VIRTUAL_FILE);
+        String originalPath = getClassPath(currentFile);
         String targetPath = project.getBasePath() + "/src/Pyz/" + originalPath;
 
+        String renderedContent = getRenderedContent(currentFile, originalPath);
+        PsiFile pyzFile = createFile(currentFile, project, renderedContent);
+
         try {
-            VirtualFile pyzDirectory = VfsUtil.createDirectoryIfMissing(targetPath);
-            Messages.showMessageDialog(project,"Directory added" + targetPath , "Greeting", Messages.getInformationIcon());
-
-            InputStream inputStream = ResourceUtil.getResourceAsStream(getClass().getClassLoader(), "templates", "phpClass.txt");
-            String content = ResourceUtil.loadText(inputStream);
-
-            String contentWithClassName = content.replace("{{className}}",e.getData(PlatformDataKeys.VIRTUAL_FILE).getNameWithoutExtension());
-            String namespace = originalPath.replace("/", "\\");
-            String contentWithClassNameAndNamespace = contentWithClassName.replace("{{namespace}}", namespace);
-
-            final PsiFileFactory factory = PsiFileFactory.getInstance(project);
-            final PsiFile file = factory.createFileFromText(e.getData(PlatformDataKeys.VIRTUAL_FILE).getName(), PhpFileType.INSTANCE, contentWithClassNameAndNamespace);
-
-            PsiDirectory baseDir = PsiDirectoryFactory.getInstance(project).createDirectory(pyzDirectory);
-            baseDir.add(file);
-
-//            OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, file.getVirtualFile());
-//            openFileDescriptor.navigate(true);
-//
-//            Editor fem = FileEditorManager.getInstance(project).openTextEditor(openFileDescriptor, true);
+            VirtualFile virtualPyzDirectory = VfsUtil.createDirectoryIfMissing(targetPath);
+            PsiDirectory pyzDirectory = PsiDirectoryFactory.getInstance(project).createDirectory(virtualPyzDirectory);
+            pyzDirectory.add(pyzFile);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
     }
 
-    private String getClassPath(@NotNull AnActionEvent e) {
-        String[] regexArray = e.getData(PlatformDataKeys.VIRTUAL_FILE).getParent().getCanonicalPath().split("(vendor\\/spryker[a-z-]*\\/[a-z-]*\\/src\\/Spryker[A-z]*\\/)");
+    @Override
+    public void update(@NotNull AnActionEvent actionEvent) {
+        VirtualFile vFile = actionEvent.getData(PlatformDataKeys.VIRTUAL_FILE);
+        if (vFile == null) {
+            actionEvent.getPresentation().setVisible(false);
+            return;
+        }
+
+        if(isFileInSprykerVendor(vFile)) {
+            actionEvent.getPresentation().setVisible(false);
+            return;
+        }
+
+        actionEvent.getPresentation().setVisible(true);
+    }
+
+    private boolean isFileInSprykerVendor(@NotNull VirtualFile vFile) {
+        return vFile.getFileType() == UnknownFileType.INSTANCE || getClassPath(vFile) == "";
+    }
+
+    @NotNull
+    private PsiFile createFile(@NotNull VirtualFile sourceFile, Project project, String renderedContent) {
+        final PsiFileFactory factory = PsiFileFactory.getInstance(project);
+
+        return factory.createFileFromText(sourceFile.getName(), PhpFileType.INSTANCE, renderedContent);
+    }
+
+    @NotNull
+    private String getRenderedContent(@NotNull VirtualFile file, String originalPath) {
+        InputStream inputStream = ResourceUtil.getResourceAsStream(getClass().getClassLoader(), "templates", "phpClass.txt");
+        String content = null;
+        try {
+            content = ResourceUtil.loadText(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String contentWithClassName = content.replace("{{className}}", file.getNameWithoutExtension());
+        String namespace = originalPath.replace("/", "\\");
+
+        return contentWithClassName.replace("{{namespace}}", namespace);
+    }
+
+    private String getClassPath(@NotNull VirtualFile file) {
+        String[] regexArray = file.getParent().getCanonicalPath().split("(vendor\\/spryker[a-z-]*\\/[a-z-]*\\/src\\/Spryker[A-z]*\\/)");
         if(regexArray.length == 2) {
             return regexArray[1];
         }
 
         return "";
-    }
-
-    @Override
-    public void update(@NotNull AnActionEvent e) {
-        VirtualFile vFile = e.getData(PlatformDataKeys.VIRTUAL_FILE);
-        if (vFile == null) {
-            e.getPresentation().setVisible(false);
-            return;
-        }
-
-        if(vFile.getFileType() == UnknownFileType.INSTANCE || getClassPath(e) == "") {
-            e.getPresentation().setVisible(false);
-        } else {
-            e.getPresentation().setVisible(true);
-        }
     }
 }
