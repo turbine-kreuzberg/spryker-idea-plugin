532ff73f4b01717e04f2947bc75f6c355fce2b4f
Jochen Gartner <jochen.gartner@turbinekreuzberg.com>
2021-09-15 09:35:47 +0200

    make PathToStubReferencesSearcher use the actual action and module name to resolve the stub

--- src/main/java/com/turbinekreuzberg/plugins/search/PathToStubReferencesSearcher.java
+++ src/main/java/com/turbinekreuzberg/plugins/search/PathToStubReferencesSearcher.java
@@ -8,6 +8,7 @@ import com.intellij.psi.search.UsageSearchContext;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.util.Processor;
 import com.jetbrains.php.lang.psi.elements.impl.MethodImpl;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class PathToStubReferencesSearcher extends QueryExecutorBase<PsiReference, ReferencesSearch.SearchParameters> {
@@ -16,10 +17,28 @@ public class PathToStubReferencesSearcher extends QueryExecutorBase<PsiReference
     @Override
     public void processQuery(ReferencesSearch.@NotNull SearchParameters queryParameters, @NotNull Processor<? super PsiReference> consumer) {
         PsiElement elementToSearch = queryParameters.getElementToSearch();
-        if (elementToSearch instanceof MethodImpl && ((MethodImpl) queryParameters.getElementToSearch()).getName().endsWith("Action")) {
+        if (elementToSearch instanceof MethodImpl && ((MethodImpl) elementToSearch).getName().endsWith("Action")) {
+
+            String moduleName = convertCamelCaseToKebabCase(getModuleName((MethodImpl) elementToSearch));
+            String methodName = convertCamelCaseToKebabCase(getMethodName((MethodImpl) elementToSearch));
+            String word = "/" + String.join("/", moduleName, "gateway", methodName);
+
             SearchRequestCollector collector = queryParameters.getOptimizer();
-//            TODO: replace static string
-            collector.searchWord("/realtime-price/gateway/get-price", elementToSearch.getUseScope(),  UsageSearchContext.IN_STRINGS, false, elementToSearch);
+            collector.searchWord(word, elementToSearch.getUseScope(),  UsageSearchContext.IN_STRINGS, false, elementToSearch);
         }
+    }
+
+    private String getModuleName(MethodImpl elementToSearch) {
+        return StringUtils.substringAfterLast(StringUtils.substringBefore(elementToSearch.getFQN(),"\\Communication\\"), "\\") ;
+    }
+
+    private String getMethodName(MethodImpl elementToSearch) {
+        return StringUtils.substringBefore(elementToSearch.getName(),"Action");
+    }
+
+    private String convertCamelCaseToKebabCase(String string) {
+        String convertedString = String.join("-", StringUtils.splitByCharacterTypeCamelCase(string));
+
+        return StringUtils.lowerCase(convertedString);
     }
 }
